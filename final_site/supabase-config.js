e563981b34721d6c4012601d9585aebbfebe8a75
r855qhpr4j-jpg <kyohei.0829.o@gmail.com>
2026-04-02 22:05:04 +0900

    Delete final_site directory

--- final_site/supabase-config.js
+++
@@ -1,7 +0,0 @@
-window.HOKUSHO_SUPABASE_CONFIG = {
-  enabled: true,
-  url: "https://flqbvihmhwzsyjqusweo.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZscWJ2aWhtaHd6c3lqcXVzd2VvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUxMDAzNDQsImV4cCI6MjA5MDY3NjM0NH0.yyaTDxCfPLSV1BOiIyIfSUXoeNZ0xhTlhCHJgPPckSg",
-  table: "hokusho_app_state",
-  scope: "shared"
-};
